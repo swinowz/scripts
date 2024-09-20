2de020f4c374b7dc98f6fafe38c0136b10040f49
swinowz <username@users.noreply.github.com>
2024-09-20 19:14:31 +0200

    idk

--- discord_bot_send_msg.py
+++ discord_bot_send_msg.py
@@ -24,30 +24,3 @@ async def on_ready():
 
 client.run(TOKEN)
 
-
-
-import discord
-
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-
-TOKEN = ''
-
-@client.event
-async def on_ready():
-    print(f'Logged in as {client.user}')
-    
-    channel_id = 1283358461282746379  # Channel ID should be an integer
-    channel = client.get_channel(channel_id)
-    
-    if channel:
-        guild = channel.guild
-        role = discord.utils.get(guild.roles, name="reminder")
-        if role:
-            await channel.send(f'test {role.mention}')
-        else:
-            print('Role named "reminder" not found')
-    else:
-        print('Channel not found')
-
-client.run(TOKEN)
