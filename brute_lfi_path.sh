14fac5377823f78ea28c6b2ce6cda1aa9b8f980e
h0lm0 <h0lm0@users.noreply.github.com>
2025-04-07 22:33:27 +0200

    header feature

--- brute_lfi_path.sh
+++ brute_lfi_path.sh
@@ -5,16 +5,18 @@ usage() {
   echo "  -u    Base URL (ex: http://site.local/page=)"
   echo "  -w    Wordlist à tester"
   echo "  -e    Message d'erreur à ignorer (ex: 'Not found')"
+  echo "  -H    Header à prendre en compte (ex: 'Authorization: Bearer test')"
   exit 1
 }
 
 [[ $# -eq 0 ]] && usage
 
-while getopts "u:w:e:" opt; do
+while getopts "u:w:H:e:" opt; do
   case $opt in
     u) URL="$OPTARG" ;;
     w) WORDLIST="$OPTARG" ;;
     e) ERROR_MSG="$OPTARG" ;;
+    H) HEADER="$OPTARG" ;;
     *) usage ;;
   esac
 done
@@ -25,19 +27,21 @@ clear
 LINES=$(tput lines)
 toggle=0
 
-# Arsenal-style colors
-COLOR1='\033[38;5;45m'   # Cyan flashy
-COLOR2='\033[38;5;201m'  # Magenta clair
+COLOR1='\033[38;5;45m'
+COLOR2='\033[38;5;201m'
 NC='\033[0m'
 
 while read -r path; do
     full_url="${URL}${path}"
 
-    # TESTING info en bas
     tput cup $((LINES - 1)) 0
     echo -ne "\033[1m\033[38;5;250mTESTING : $path\033[K${NC}"
 
-    response=$(curl -s "$full_url")
+    if [[ -n "$HEADER" ]]; then
+        response=$(curl -s -H "$HEADER" "$full_url")
+    else
+        response=$(curl -s "$full_url")
+    fi
 
     if [[ ! "$response" =~ $ERROR_MSG && -n "$response" ]]; then
         if [[ $toggle -eq 0 ]]; then
@@ -56,6 +60,5 @@ while read -r path; do
     fi
 done < "$WORDLIST"
 
-# Curseur propre à la fin
 tput cup $LINES 0
 echo ""
